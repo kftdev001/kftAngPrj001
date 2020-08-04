914bd5bc187f8bdd28f34d6b1e4e075fd8abd3aa
kftdev001 <kftdev001@gmail.com>
2020-08-04 13:26:46 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent  
+  // ProductModule は不要？？
   ],
   imports: [
     BrowserModule,
---
+++ src/app/protucts.ts
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    coverImage: "./assets/img/img01.jpg",
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'Phone XL サンプルheading1',
+    heading2: 'Phone XL サンプルheading2',
+    heading3: 'Phone XL サンプルheading3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキストサンプルテキスト サンプルテキスト サンプルテキストサンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+    headingtext3: 'サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト'
+
+
+  },
+  {
+    coverImage: "./assets/img/img02.jpg",
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'Phone Mini heading1',
+    heading2: 'Phone Mini heading2',
+    heading3: 'Phone Mini heading3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+    headingtext3: 'サンプルテキストサンプルテキストサンプルテキスト'
+
+  },
+  {
+    coverImage: "./assets/img/img03.jpg",
+    name: 'Phone Standard',
+    price: 299,
+    description: 'description for Phone Standard'
+  },
+  {
+    coverImage: "./assets/img/phone-cover.jpg",
+    name: 'Phone Special',
+    price: 999,
+    description: 'long description for Phone Special aaaaaaaaaaaaaabbbbbbbbbbb'
+  }
+];
